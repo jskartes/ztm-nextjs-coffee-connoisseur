073e72280ccab0dc950fca77257adb9224965ef5
jskartes <joshuakartes@gmail.com>
2024-05-22 12:13:15 -0600

    set homepage background mesh gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-between p-14 bg-purple-951"
+      className="flex min-h-screen flex-col items-center justify-between p-14"
     >
       <h1>Coffee Connoisseur</h1>
     </main>
